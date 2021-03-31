ee26f3389049d709aee4b62d2603415b565f7779
lisswart <arnoudlisa@gmail.com>
2021-03-31 06:29:08 -0400

    commit tree 46b8392deec7d4c7aa88191134edfac930600b5c

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Meg';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'Shaw';
+}
